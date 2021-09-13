4e4232eaf6ae929115769ecce61db929a35d6b3d
cneralich <corrigan@hashicorp.com>
2021-09-13 10:38:30 -0700

    Create main.tf

---
+++ tag_module/main.tf
@@ -0,0 +1,22 @@
+data "tfe_workspace" "test" {
+  name         = var.TFC_WORKSPACE_NAME
+  organization = split("/",var.TFC_WORKSPACE_SLUG)[0]
+}
+
+variable "TFC_WORKSPACE_NAME" {}
+variable "TFC_WORKSPACE_SLUG" {}
+
+locals {
+  single_tags = {
+    for tag in data.tfe_workspace.test.tag_names : format("%s-%s","tag",tag) => tag
+    if length(regexall("[:]+", tag)) == 0
+  }
+  compound_tags = {
+    for tag in data.tfe_workspace.test.tag_names : split(":",tag)[0] => split(":",tag)[1]
+    if length(regexall("[:]+", tag)) > 0
+  }
+}
+
+output "tags" {
+    value = merge(local.single_tags, local.compound_tags)
+}
